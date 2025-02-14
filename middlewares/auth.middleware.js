a56e6e9c999c4c11e52f318ebd4a416542e7b648
Habert kungu <habert231@gmail.com>
2025-02-14 09:49:49 +0300

    adding authorization and securing api

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { JWT_SECRET } from "../config/env.js";
+
+const authorize = async (req, res, next) => {
+  try {
+    let token;
+    //find the user based of of the token of the  user that is making the request
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    //looks up and decodes it
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    const decoded = jwt.vertify(token, JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized", error: error.message });
+  }
+};
+export default authorize;
