6aa26f9577b79e67109d4e5df6a2b859129da063
Habert kungu <habert231@gmail.com>
2025-02-14 10:54:57 +0300

    added bot detection and rate limiting using arcjet

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 import express, { Route } from "express";
+import { PORT } from "./config/env.js";
+import cookieParser from "cookie-parser";
+
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.router.js";
 import authRouter from "./routes/auth.routes.js";
-import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
-import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
-
-app.use(express.json());
-//Routes
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
 app.use(express.urlencoded({ extended: false })); //process the form data
 app.use(cookieParser());
+app.use(arcjetMiddleware);
+app.use(express.json());
+
+//Routes
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
--- config/env.js
+++ config/env.js
@@ -3,5 +3,12 @@ import { config } from "dotenv";
 //pull from node env or default to the development env
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
-  process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_KEY,
+  ARCJET_ENV,
+} = process.env;
